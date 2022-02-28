785fdc85c63204aa9b802cbd7eaea4eb328cac91
DahVoid <cederlund.oscar@gmail.com>
2022-02-28 11:51:38 +0100

    Merge, inheritance for records and add funcs

--- assignment_2/symbol_table.cpp
+++ assignment_2/symbol_table.cpp
@@ -3,77 +3,225 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <map>
+#include <list>
 
 using namespace std;
 
-struct Var 
+class Record
 {
+  public:
+    string id;
     string type;
-    string name;
-};
-
-struct Meth 
-{
-    string type;
-    string name;
-};
-
-struct node 
-{
-    string name;
-    string type;
-    vector<node> children;
-    
-};
-
-vector<node> tree;
-
-int symbol_table(Node* root, int depth=0) 
-{
-    Node* child;
-
-    for (auto i = root->children.begin(); i != root->children.end(); i++)
+    Record(string id, string type)
     {
-        for(int i=0; i<depth; i++)
-        cout << "    ";
-        child = *i;
-        cout << "Type: " <<  child->type << "   Value: " << child->value << "\n";
-        symbol_table(child, depth+1);
-    }
+        this.id = id;
+        this.type = type;
+        cout << "contructed: " << type << endl;
+    };
+
+
+  void printRecord()
+  {
+    cout << "id: " << id << "\n" << "type: " <<  type << "\n";
+  }
+};
+
+class Container: public Record
+{
+    public:
+        map<string, Variable> variables;
+        Container(string id, string type) : Record(id, type)
+        {
+
+        };
+
+        void addVariable(string var_id)
+        {
+            new_var = new Variable(var_id, "Variable");
+            //Write code here:
+            variables.insert(pair<string ,Variable>(var_id, new_var));
+        }
+ 
+
 }
 
+class Variable: public Record
+{
+    public:
+        // Constructor
+        Variable(string id, string type) : Record(id, type)
+        {
+
+        };
+
+};
+
+class Method: public Container
+{
+  public: 
+    map<string, Variable> parameters;
+    Method(string id, string type) : Container(id, type)
+    {
+    };  
+
+  void addParameter(string par_id)
+  {
+    //Write code here:
+    new_par = new Variable(par_id, "Variable");
+    //Write code here:
+    parameters.insert(pair<string ,Variable>(par_id, new_par));
+
+  }
+};
+
+
+class Class: public Container
+{
+  public:
+    map<string, Method> methods;
+    Class(string id, string type) : Container(id, type)
+    {
+
+    };
+    
+
+  void addMethod(string met_id)
+  {
+    //Write code here:
+    new_met = new Method(met_id, "Method");
+    //Write code here:
+    methods.insert(pair<string ,Method>(par_id, new_met));
+
+
+  }
+
+  string lookupVariable()
+  {
+    //Write code here:
+
+  }
+
+  string lookupMethod()
+  {
+    //Write code here:
+
+  }
+};
 
 class Scope
 {
-    int next = 0; // Next child to visit 
-    Scope parentScope; // parent scope
-    list childrenScopes;
-    map records; //Symbol to record map
-    // ...
+  public:
+    int next = 0; //next child to visit
+    // Scope* parentScope; //parent scope
+    list<Scope> childrenScopes; //children scopes
+    map<string, Record> records; //symbol to record map
 
-    Scope nextChild()
+    Scope(Scope* parentScope_in)
     {
-        Scope nextChild;
-        if (next == childrenScopes.size())
-        {   // Create new child Scope
-            nextChild = new Scope(this);
-            childrenScopes.add(nextChild);
-        }
-        else
-        {
-            nextChild = childrenScopes.get(next); // visit scope
-            next++;
-            return nextChild;
-        }
-
-        Record lookup(string Key)
-        {
-            if(record.containsKey(Key)) //does it exists in the current scope?
-            {  
-                return record.get(Key);
-            }
-        }
+      Scope parentScope = parentScope_in;
     }
 
+    Scope()
+    {
+
+    }
+
+    Scope next_Child()
+    {
+      if(next == childrenScopes.size()) // create new child scopre
+      {
+        Scope nextChild(this); // VIKTIGT ------> insert "this" i in parameter till scope <--- VIKTIGT
+        childrenScopes.push_back(nextChild);
+      }
+
+      else
+      {
+        Scope nextChild = childrenScopes[next]; //visit scope
+      }
+      next++;
+      return nextChild;
+    }
+
+    Record lookup(string Key)
+    {
+      if(records.containskey(Key)) //does it exist in the current scope? Använd map.count (records.count)
+      {
+        return record.get(Key);
+      }
+
+      else
+      {
+        if (parent == null)
+        {
+          return null; //identifier not in the symbol table
+        }
+
+        else
+        {
+          return parent.lookup(Key); // delegate the request to the parent scope
+        }
+      }
+    }
+
+    void resetScope()
+    {
+      next = 0;
+      for(int i = 0; i < children.size(); i++)
+      {
+        children.get(i).resetScope();
+      }
+    }
+};
+
+
+
+class SymbolTable
+{
+  Scope root;
+  Scope current;
+
+  void SymbolTable()
+  {
+    root = new Scope(null);
+    current = root;
+  }
+
+  void enterScope()
+  {
+    current = current.nextChild(); //create new scope if needed
+  }
+
+  void exitScope()
+  {
+    current = current.parent();
+  }
+
+
+  void put(String Key, Record item)
+  {
+    current.put(Key, item);
+  }
+
+  Record lookup(String Key)
+  {
+    return current.lookup(Key);
+  }
+
+
+  void printTable()
+  {
+    root.printScope();
+  }
+
+  void resetTable()
+  {
+    root.resetScope();
+  }
+};
+
+
+int main()
+{
+  return 0;
 }
